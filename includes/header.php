b5d8bb67c295c00f65f0d671ee9225f69ff87530
oyeto223-star <oyetoundesimon@gmail.com>
2026-07-08 20:52:17 +0100

    Ajouter le header

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Stock - Équipe de Football</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="navbar-container">
+            <div class="navbar-logo">
+                <h1>⚽ Gestion Stock Football</h1>
+            </div>
+            <ul class="nav-menu">
+                <li><a href="index.php" class="nav-link">Accueil</a></li>
+                <li><a href="pages/inventaire.php" class="nav-link">Inventaire</a></li>
+                <li><a href="pages/ajouter_produit.php" class="nav-link">Ajouter Produit</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
