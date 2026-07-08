4ab63747485566b90c1fc09360bac1ed4ca2bb69
oyeto223-star <oyetoundesimon@gmail.com>
2026-07-08 20:52:45 +0100

    Ajouter le footer

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<footer class="footer">
+    <div class="footer-content">
+        <p>&copy; 2024 Gestion Stock Football - Tous droits réservés</p>
+        <p>Système de gestion d'inventaire pour équipe de football</p>
+    </div>
+</footer>
+</body>
+</html>
